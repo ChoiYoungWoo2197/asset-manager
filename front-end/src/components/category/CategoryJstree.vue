4b162b65974024304ca7eb74e43275c3d57905c7
Choi Young Woo <duddn2197@cmtinfo.co.kr>
2021-07-26 18:30:30 +0900

    카테고리 1. category, categoryJstree 구현.

---
+++ front-end/src/components/category/CategoryJstree.vue
@@ -0,0 +1,158 @@
+<template>
+  <div id="category-div">
+  </div>
+</template>
+
+<script>
+import $ from "jquery";
+import axios from "axios";
+
+export default {
+  name: "CategoryJstree",
+  data() {
+    return {
+      activeMode : 'create',
+      datas : null,
+    }
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    getInstance() {
+      return $("#" + "category-div");
+    },
+    init() {
+      const vm = this;
+      this.getInstance().jstree({
+        "core" : {
+          "animation" : 0,
+          "themes" : { "stripes" : true },
+          'check_callback': true,
+          "multiple" : false,
+          'data' : {
+            'url' : vm.getJstreeData(),
+            'data' : function (node) {
+              return { 'id' : node.id };
+            }
+          },
+          'strings' : {
+            'Loading ...' : '불러오는 중 ...'
+          },
+        },
+        "types" : {
+          "root" : {
+            "icon" : "uil uil-folder-lock",
+          },
+          "Dept" : {
+            "icon" : "uil uil-folder-lock",
+            "valid_children" : ["Dept"]
+          }
+        },
+        "contextmenu":{
+          'select_node': false, //우클릭시 노드가 선택을 않기 위해
+          "items": function($node) {
+            return vm.getMenus($node);
+          }
+        },
+        "plugins" : vm.plugins(),
+      }).on("refresh.jstree", function () {
+        vm.getInstance().jstree(true).open_all();
+      }).on("rename_node.jstree", function (event, renamed_node) {
+        if(vm.activeMode === 'create') {
+          vm.createBtnClick(renamed_node.node);
+        }
+      }).on("select_node.jstree", function (event, selected_node) {
+        if(selected_node.node.id === "0" || selected_node.event === undefined) return false;
+        vm.$emit("changedCategoryActive", selected_node.node);
+      });
+    },
+    getJstreeData() {
+      const vm = this;
+      axios.get('http://localhost:8080/api/categorys', {}
+      ).then(response => {
+        vm.datas = [];
+        vm.datas = response.data.slice(0);
+        let jstreeDatas = [];
+        jstreeDatas.push({
+          "id": "0", "parent": "#", "text": "전체 카테고리 목록 (code/name)" , "type" : "root",
+          "a_attr": {
+            class: "disable_checkbox"
+          }
+        });
+
+        vm.datas.forEach((category) => {
+          if(category.useYn === true) {
+            jstreeDatas.push({
+              "id" : category.id,
+              "parent" : category.parentId === null?0: category.parentId,
+              "text" :  category.code + " / " + category.name,
+              "type" : "Dept",
+            });
+          }
+        })
+
+        vm.getInstance().jstree(true).settings.core.data = jstreeDatas;
+        vm.getInstance().jstree(true).refresh();
+      }).catch(e => {
+        alert(e);
+      })
+
+    },
+    plugins() {
+      var plugins = ["contextmenu", "types", "wholerow", "search"];
+      return plugins;
+    },
+    getMenus($node){
+      const vm = this;
+      let menus = {};
+      let jsTreeInstance = vm.getInstance().jstree(true);
+      $.extend(menus, {
+        "Create": {
+          "separator_before": false,
+          "separator_after": false,
+          "label": "생성",
+          "action": function () {
+            $node = jsTreeInstance.create_node($node, {"text" : "code", "type": "Dept"});
+            jsTreeInstance.edit($node);
+            vm.activeMode = "create";
+          }
+        }
+      });
+      return menus;
+    },
+    createBtnClick(node) {
+      const vm = this;
+      axios.get('http://localhost:8080/api/categorys/' + node.text + '/exists')
+          .then(response => {
+            if(response.status === 200) {
+              if(response.data === true) {
+                alert("중복된 코드입니다.");
+                vm.getJstreeData();
+              } else {
+                axios.post('http://localhost:8080/api/categorys', {
+                  parentId : node.parent === "0" ? null : node.parent,
+                  name : node.text,
+                  code : node.text,
+                  remark : "",
+                  useYn : true,
+                  register : "1",
+                }).then(response => {
+                  console.log(response);
+                  vm.getJstreeData();
+                }).catch(e => {
+                  alert(e);
+                });
+              }
+            }
+          }).catch(e => {
+        alert(e);
+      });
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ front-end/src/views/Category.vue
@@ -0,0 +1,144 @@
+<template>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h4 class="m-0"> 카테고리 정보관리 <small></small></h4>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">기초정보</a></li>
+              <li class="breadcrumb-item active">카테고리 정보관리</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container">
+        <div class="mb-2">
+          <div class="text-right">
+            <div v-if="activeVueComponent === 'category'">
+<!--              <button class="file-upload btn btn-secondary btn-sm mr-1">
+                <i class="far fa-plus-square pr-1"></i>등록
+              </button>-->
+            </div>
+            <div v-else>
+              <button class="create btn btn-secondary btn-sm mr-1" @click="showCreateModel">
+                <i class="far fa-plus-square pr-1"></i>등록
+              </button>
+              <button class="file-upload btn btn-secondary btn-sm mr-1">
+                <i class="fas fa-file-download pr-1"></i>파일업로드
+              </button>
+              <button class="btn btn-secondary btn-sm">
+                <i class="fas fa-file-export pr-1"></i>파일다운로드
+              </button>
+            </div>
+
+
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="card card-primary card-outline card-tabs h-100">
+              <div class="card-header">
+                <h3 class="card-title"></h3>
+              </div>
+              <div class="card-body">
+                <div class="card-body table-responsive p-0 table-hover" style="height: 500px;">
+                  <form>
+                    <div class="form-group">
+                      <CategoryJstree ref="categoryJstree"
+                                        @changedCategoryActive="handleChangedCategoryActive">
+                      </CategoryJstree>
+                    </div>
+                  </form>
+
+                </div>
+              </div>
+              <div class="card-footer clearfix">
+              </div>
+            </div><!-- /.card -->
+          </div>
+          <div class="col-lg-8">
+            <div class="card card-primary card-outline card-tabs h-100">
+              <div class="card-header p-0 pt-1 border-bottom-0 ">
+                <ul class="nav nav-tabs" id="custom-tabs-three-tab" role="tablist">
+                  <li class="nav-item">
+                    <a class="nav-link active" id="custom-tabs-three-category-tab" data-toggle="pill" href="#custom-tabs-three-category"
+                       role="tab" aria-controls="custom-tabs-three-category" aria-selected="true" @click="clickTab('category')">카테고리 관리</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" id="custom-tabs-three-category-specification-tab" data-toggle="pill" href="#custom-tabs-three-category-specification"
+                       role="tab" aria-controls="custom-tabs-three-custom-tabs-three-category-specification" aria-selected="false" @click="clickTab('spcs')">규격 관리</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="card-body" style="height: 500px;">
+                <div class="tab-content" id="custom-tabs-three-tabContent">
+                  <div class="tab-pane fade active show" id="custom-tabs-three-dept" role="tabpanel" aria-labelledby="custom-tabs-three-category-tab">
+<!--                    <DepartmentEditor ref="departmentEditor" :p-department="activeDepartment" @updateData="handleUpdateData"></DepartmentEditor>-->
+                  </div>
+                  <div class="tab-pane fade" id="custom-tabs-three-member" role="tabpanel" aria-labelledby="custom-tabs-three-category-specification-tab">
+<!--                    <Member ref="member" :p-department="activeDepartment"></Member>-->
+                  </div>
+                </div>
+              </div>
+              <!-- /.card -->
+            </div>
+          </div>
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+
+
+  </div>
+  <!-- /.content-wrapper -->
+</template>
+<script>
+import CategoryJstree from "@/components/category/CategoryJstree.vue";
+
+export default {
+  name: "Category",
+  components: {
+    CategoryJstree
+  },
+  data() {
+    return {
+      activeVueComponent : 'category',
+      activeCategory : null,
+    }
+  },
+  mounted() {
+    document.body.classList.remove('login-page');
+    document.body.classList.add('layout-top-nav');
+  },
+  methods: {
+    handleChangedCategoryActive(data) {
+      this.activeCategory = data;
+    },
+    handleUpdateData() {
+      this.activeCategory = null;
+      // this.$refs.departmentJstree.getJstreeData();
+    },
+    clickTab(activeVueComponent) {
+      this.activeVueComponent = activeVueComponent;
+    },
+    showCreateModel() {
+      // this.$refs.member.showCreateModal();
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
